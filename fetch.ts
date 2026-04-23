25b3bc6c49a062df8ce50974205e94520a890e1a
Sami <saifsaba0987@gmail.com>
2026-04-23 23:44:39 +0600

    Delete fetch.ts

--- fetch.ts
+++
@@ -1,2 +0,0 @@
-import https from 'https';
-https.get('https://raw.githubusercontent.com/Sami001-OG/Multi-Confluence-Scalper-btc-eth-sol-/main/server.ts', res => { let d=''; res.on('data', c=>d+=c); res.on('end', ()=>console.log(d)) });
